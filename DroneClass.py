5c84727997559d0b62106230cbe5fddcbfc81998
feibi-allen <sc23fba@leeds.ac.uk>
2024-07-19 14:16:43 +0100

    checking for passing endpoint introduced

--- DroneClass.py
+++ DroneClass.py
@@ -1,15 +1,18 @@
 import simpy
 
-TIME_STEP = 1 # Amount of time passed between each position update
+TIME_STEP = 1  # Amount of time passed between each position update
+MAX_SPEED = 10 # Speed in ms-1
 
 class Drone(object):
-    def __init__(self,env , start, end, velocity, id):
+    def __init__(self, env, start, end, velocity, id):
+        # FIXME - assert check velocity (try except)
         self.env = env
         self.start = start
         self.end = end
         self.velocity = velocity
         self.pos = self.start
         self.id = id
+        self.moving_time = 0
         #self.fly_process = env.process(self.fly())
         #env.process(self.stopping)
 
@@ -32,14 +35,19 @@ class Drone(object):
         return self.id
 
     def fly(self):
-        while True: # FIXME - should be changed to while not finished
+        while not self.end_point_passed():
             time_to_move = TIME_STEP
             while time_to_move:
                 try:
                     yield self.env.timeout(time_to_move)
+                    time_to_move = 0
                 except simpy.Interrupt:
                     self.check_if_go()
-
+            self.moving_time += TIME_STEP
+            self.pos[0], self.pos[1] = self.pos[0] + (
+                    self.velocity[0] * self.moving_time), self.pos[1] + (
+                                               self.velocity[
+                                                   1] * self.moving_time)
 
     def stop(self):
         # check for collisions
@@ -49,6 +57,16 @@ class Drone(object):
         # repeat
         pass
 
+    def end_point_passed(self):
+        # check collinearity of start end and current pos
+        if (self.end[0] - self.start[0]) * (self.pos[1] - self.start[1]) != (self.end[1] - self.start[1]) * (self.pos[0] - self.start[0]):
+            return False
+        # check within bounds of start and end points
+        if not(min(self.start[0], self.end[0]) <= self.pos[0] <= max(self.start[0], self.end[0])):
+            return False
+        if not (min(self.start[1], self.end[1]) <= self.pos[1] <= max(self.start[1], self.end[1])):
+            return False
+        return True
     def permission_to_move(self):
         # collision resolve
         pass
