9406d9c944d614aca6910cf004bcf3c19b8032b5
feibi-allen <sc23fba@leeds.ac.uk>
2024-07-19 12:10:34 +0100

    fly and stop processed added not implemented

--- DroneClass.py
+++ DroneClass.py
@@ -1,13 +1,17 @@
 import simpy
 
+TIME_STEP = 1 # Amount of time passed between each position update
 
 class Drone(object):
-    def __init__(self, start, end, velocity, id):
+    def __init__(self,env , start, end, velocity, id):
+        self.env = env
         self.start = start
         self.end = end
         self.velocity = velocity
         self.pos = self.start
         self.id = id
+        self.fly_process = env.process(self.fly())
+        env.process(self.stopping)
 
     def finished(self):
         return self.pos == self.end
@@ -26,3 +30,25 @@ class Drone(object):
 
     def get_id(self):
         return self.id
+
+    def fly(self):
+        while True: # FIXME - should be changed to while not finished
+            time_to_move = TIME_STEP
+            while time_to_move:
+                try:
+                    yield self.env.timeout(time_to_move)
+                except simpy.Interrupt:
+                    self.check_if_go()
+
+
+    def stop(self):
+        # check for collisions
+        # if no collisions within time boundary check again and repeat
+        # timeout for collisions
+        # interrupt after timeout
+        # repeat
+        pass
+
+    def permission_to_move(self):
+        # collision resolve
+        pass
