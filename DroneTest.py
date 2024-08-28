5a5626c62d59fbc04d9e5f82ee3501f3204b3aaf
feibi-allen <sc23fba@leeds.ac.uk>
2024-08-28 12:59:22 +0100

    drone testing - param values

--- DroneTest.py
+++ DroneTest.py
@@ -16,4 +16,25 @@ class DroneTest(unittest.TestCase):
         with self.assertRaises(TypeError):
             Drone(1, [1, 2, 3], [1, 2, 4], "a","a")
 
+    def test_param_value_checks(self):
+        with self.assertRaises(TypeError):
+            Drone("a", [1, 2, 3], [1, 2, 4], "a")
 
+        with self.assertRaises(TypeError) as context:
+            Drone(1, [2, 3], [1, 2, 4], "a")
+        self.assertEqual(str(context.exception),
+                         "Start and end must be in form [x,y,z]")
+
+        with self.assertRaises(TypeError) as context:
+            Drone(1, "[2, 3]", [1, 2, 4], "a")
+        self.assertEqual(str(context.exception),
+                         "Start and end must be in form [x,y,z]")
+
+        with self.assertRaises(TypeError) as context:
+            Drone(1, [1,2, 3], "[1, 2, 4]", "a")
+        self.assertEqual(str(context.exception),
+                         "Start and end must be in form [x,y,z]")
+
+        with self.assertRaises(TypeError) as context:
+            Drone(1, ["a", 2, 3], [1, 2, 4], "a")
+        self.assertEqual(str(context.exception),"[x,y,z] must be numeric")
