a8433d5bd197242e02bfb26f655d90cd1b7a28fb
feibi-allen <sc23fba@leeds.ac.uk>
2024-10-07 21:53:52 +0100

    simulation was not running long enough

--- RunAirspace.py
+++ RunAirspace.py
@@ -9,8 +9,10 @@ env = simpy.Environment()
 
 airspace = Airspace(env)
 
-Drone(speed=5, start=[0, 0, 0], end=[10, 10, 0],
+Drone(speed=0.5, start=[0, 0, 0], end=[10, 10, 0],
       airspace=airspace, id_str="c")
-Drone(speed=3, start=[2, 0, 0], end=[10, 10, 0],
+Drone(speed=0.7, start=[2, 0, 0], end=[10, 10, 0],
       airspace=airspace, id_str="d")
-env.run(until=20)
+Drone(speed=1, start=[2, 5, 0], end=[10, 20, 0],
+      airspace=airspace, id_str="e")
+env.run(until=50)
