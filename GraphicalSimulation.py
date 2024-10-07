23611795cecd507e90a4c59355b9443960573b61
feibi-allen <sc23fba@leeds.ac.uk>
2024-10-07 21:22:51 +0100

    basic graphic simulation added. not tested

--- GraphicalSimulation.py
+++ GraphicalSimulation.py
@@ -1,4 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-from matplotlib.animation import FuncAnimation
+
+
+class Simulation:
+    def __init__(self, limits):
+        self.fig = plt.figure()
+        self.axis = self.fig.add_subplot(111, projection='3d')
+        self.axis.set_xlim(limits[0])
+        self.axis.set_ylim(limits[1])
+        self.axis.set_zlim(limits[2])
+        self.scat = self.axis.scatter([], [], [])
+
+    def update_plot(self, drones):
+        positions = [drone.get_pos() for drone in drones]
+        x_coords, y_coords, z_coords = zip(*positions)
+        self.scat._offsets3d = (x_coords, y_coords, z_coords)
+        plt.show()
